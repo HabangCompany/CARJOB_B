a6b119df58f6744de99e3ee2a1a2db64e85b1ebf
하방방 <81242485+jumosd@users.noreply.github.com>
2024-10-15 12:40:19 +0900

    회원가입 모델들 수정- 이름,주소 숫자늘림

--- accounts/models.py
+++ accounts/models.py
@@ -7,9 +7,9 @@ from carStore.models import CarStore
 #유저 주소
 class UserAddress(models.Model):
     postal_code = models.CharField(max_length=10, blank=False)  # 우편번호
-    street_address = models.CharField(max_length=30, blank=False)  # 도로명 주소
-    jibun_address = models.CharField(max_length=30, blank=True, null=True)  # 지번 주소
-    detail_address = models.CharField(max_length=30, blank=False)  # 상세 주소
+    street_address = models.CharField(max_length=50, blank=False)  # 도로명 주소
+    jibun_address = models.CharField(max_length=50, blank=True, null=True)  # 지번 주소
+    detail_address = models.CharField(max_length=50, blank=False)  # 상세 주소
 
     def __str__(self):
         return f"{self.street_address}, {self.postal_code}"
@@ -17,11 +17,11 @@ class UserAddress(models.Model):
     
 class User(AbstractUser):
     email =models.EmailField(unique=True) # 유일한 이메일 입력
-    name = models.CharField(max_length=5, blank=False)
+    name = models.CharField(max_length=20, blank=False)
     phoneNumber = models.CharField(max_length=15, blank=False)
     # User와 UserAddress 1:1 관계 설정
     address = models.OneToOneField(UserAddress, on_delete=models.CASCADE, null=True, blank=True)
-    carModel = models.CharField(max_length=10, blank=True)
+    carModel = models.CharField(max_length=20, blank=True)
     is_Store = models.BooleanField(default=False)
     store= models.ForeignKey(CarStore, on_delete=models.CASCADE,null=True, blank=True)
     first_name = None
