860939e2bdd60a035e428e1964c3d9ff83dea45f
하방방 <81242485+jumosd@users.noreply.github.com>
2024-10-18 09:45:04 +0900

    변수명 스네이크 케이스로 변경

--- carStore/serializers.py
+++ carStore/serializers.py
@@ -14,22 +14,22 @@ class SigongSkillSerializer(serializers.ModelSerializer):
 class CarStoreSerializer(serializers.ModelSerializer):
     storeLocation = LocationSerializer()  # Nested serializer
     sigongSkill = SigongSkillSerializer()  # Nested serializer
-    thumbnail = serializers.ImageField(required=False)
+    thumbnail = serializers.ImageField(required=False) ##필수 항목이아님
 
     class Meta:
         model = CarStore
-        fields = ['storeName', 'storeLocation', 'sigongSkill', 'thumbnail', 'business_hours', 'holidays', 'store_phone']
+        fields = ['store_name', 'store_location', 'sigong_skill', 'thumbnail', 'business_hours', 'holidays', 'store_phone']
 
     def create(self, validated_data):
         # 먼저 Location 및 SigongSkill 데이터를 가져옴
-        location_data = validated_data.pop('storeLocation')
-        sigong_skill_data = validated_data.pop('sigongSkill')
+        location_data = validated_data.pop('store_location')
+        sigong_skill_data = validated_data.pop('sigong_skill')
 
         # 중첩된 데이터를 이용해 Location 및 SigongSkill 객체 생성
         location = Location.objects.create(**location_data)
         sigong_skill = SigongSkill.objects.create(**sigong_skill_data)
 
         # CarStore 객체를 생성하고 위에서 만든 객체를 연결
-        car_store = CarStore.objects.create(storeLocation=location, sigongSkill=sigong_skill, **validated_data)
+        car_store = CarStore.objects.create(store_location=location, sigong_skill=sigong_skill, **validated_data)
 
         return car_store
