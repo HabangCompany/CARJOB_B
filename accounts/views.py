92f1158e95da0a0dfd46ac313523e993a743bc69
하방방 <81242485+jumosd@users.noreply.github.com>
2024-11-11 23:15:19 +0900

    Update views.py

--- accounts/views.py
+++ accounts/views.py
@@ -4,15 +4,6 @@ from rest_framework.views import APIView
 from rest_framework.decorators import api_view
 from .serializers import UserSerializer, LoginSerializer
 
-# class RegisterUserView(APIView):
-#     def post(self, request):
-#         serializer = UserSerializer(data=request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data, status=status.HTTP_201_CREATED)
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-#회원가입
 @api_view(['POST'])
 def register_user(request):
     serializer = UserSerializer(data = request.data)
